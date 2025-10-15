81b70a2c59964c16b561af150d2dedcb9805b4bf
Erik André Rognaldsen <erik.rognaldsen@oneco.no>
2025-10-15 10:58:28 +0200

    test

--- src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
@@ -11,7 +11,7 @@ public class DayMain {
 
         double[] powerprices_day = DayPowerData.powerprices_day;
 
-        System.out.println("==============");
+        System.out.println("==============HALLO");
         System.out.println("OPPGAVE 1");
         System.out.println("==============");
         System.out.println();
